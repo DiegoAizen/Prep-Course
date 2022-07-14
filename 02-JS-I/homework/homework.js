9f2945cb5351efb27e58cbd15c9af35f9d60298e
DiegoAizen <109271780+DiegoAizen@users.noreply.github.com>
2022-07-14 03:18:26 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = hola_mundo;
 
 // Crea una variable booleana:
 const nuevoBool = null;
